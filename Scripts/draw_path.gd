4200704edcd7c6b521ac4a37700fe1833e34b904
mrfish233 <baboom003@gmail.com>
2024-05-19 05:58:52 +0800

    feat: able to check path

--- Scripts/draw_path.gd
+++ Scripts/draw_path.gd
@@ -1,9 +1,8 @@
 extends TileMap
 
-const UP = 0
-const DOWN = 1
-const LEFT = 2
-const RIGHT = 3
+enum Direction {UP, DOWN, LEFT, RIGHT}
+
+enum MapType {ROAD, WALL, DOOR, BUTTON}
 
 const X = 0
 const Y = 1
@@ -14,14 +13,17 @@ const dy = [0, 0, -1, 1]
 const layer = 0
 const SOURCE_ID = 0
 
-const TILE_NULL = Vector2i(1, 0)
-const TILE_PATH = Vector2i(0, 0)
+const TILE_NULL = Vector2i(0, 0)
+const TILE_PATH = Vector2i(1, 0)
+const TILE_CORRECT = Vector2i(2, 0)
+const TILE_WRONG   = Vector2i(3, 0)
 
 var pos  = [0,0]
 var path = [[0,0]]
 var map
 var row
 var col
+var end = false
 
 func _ready():
 	map = Global.select_map(Global.map)
@@ -30,14 +32,17 @@ func _ready():
 	_draw_empty_maze()
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_up"):
-		_move(UP)
-	elif Input.is_action_just_pressed("ui_down"):
-		_move(DOWN)
-	elif Input.is_action_just_pressed("ui_left"):
-		_move(LEFT)
-	elif Input.is_action_just_pressed("ui_right"):
-		_move(RIGHT)
+	if not end:
+		if Input.is_action_just_pressed("ui_up"):
+			_move(Direction.UP)
+		elif Input.is_action_just_pressed("ui_down"):
+			_move(Direction.DOWN)
+		elif Input.is_action_just_pressed("ui_left"):
+			_move(Direction.LEFT)
+		elif Input.is_action_just_pressed("ui_right"):
+			_move(Direction.RIGHT)
+		elif Input.is_action_just_pressed("reset"):
+			_reset_path()
 
 func _draw_empty_maze():
 	var width  = get_viewport().size.x
@@ -61,13 +66,36 @@ func _draw_cell(pos: Vector2, type: Vector2i):
 	set_cell(layer, pos, SOURCE_ID, type)
 
 func _move(dir):
-	if _is_valid_pos(dir):
-		pos[X] += dx[dir]
-		pos[Y] += dy[dir]
-		path.push_back([pos[X], pos[Y]])
-		_draw_cell(Vector2(pos[Y], pos[X]), TILE_PATH)
-	else:
+	if not _is_valid_pos(dir):
 		print("Block!")
+		return
+	
+	pos[X] += dx[dir]
+	pos[Y] += dy[dir]
+	path.push_back([pos[X], pos[Y]])
+	_draw_cell(Vector2(pos[Y], pos[X]), TILE_PATH)
+	
+	if (pos[X] == row - 1 && pos[Y] == col - 1):
+		_check_path()
+		end = true
+
+func _reset_path():
+	path.clear()
+	path.append([0, 0])
+	pos = [0, 0]
+	_draw_empty_maze()
+
+func _check_path():
+	for i in range(path.size()):
+		var x = path[i][X]
+		var y = path[i][Y]
+		var tile
+		if (map[x][y] == 0 || map[x][y] == 2):
+			tile = TILE_CORRECT
+		else:
+			tile = TILE_WRONG
+		
+		_draw_cell(Vector2(y, x), tile)
 
 func _is_valid_pos(dir):
 	var tmp = [0,0]
