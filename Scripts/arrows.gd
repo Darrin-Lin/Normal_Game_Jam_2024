9fd453a344da529e91f4ff058fae06ea6648774a
Darrin-Lin <darrin.de88@gmail.com>
2024-05-19 05:17:46 +0800

    feat: Add button and modify const to enum.

--- Scripts/arrows.gd
+++ Scripts/arrows.gd
@@ -5,10 +5,9 @@ var down_arrow
 var left_arrow
 var right_arrow
 
-const UP = 0
-const DOWN = 1
-const LEFT = 2
-const RIGHT = 3
+enum Direction {UP, DOWN, LEFT, RIGHT}
+
+enum MapType {ROAD, WALL, DOOR, BUTTON}
 
 const X = 0
 const Y = 1
@@ -21,12 +20,16 @@ var pos = [0,0]
 var row
 var col
 
-var color_white = "0xffffff"
-var color_red = "0xff0000"
-var color_blue = "0x0000ff"
+var button_pressed:bool = false
+
+var normal_color = Vector4(1,1,1,1)
+const error_color = Vector4(1,0,0,1)
+const sucess_color = Vector4(0,1,0,1)
+const door_color = Vector4(0,0,1,1)
+
 
 func _ready():
-	map = $"../Maze/Maps".map_eazy
+	map = Global.select_map(Global.map)
 	row = map.size()
 	col = map[0].size()
 	up_arrow = $UpArrow
@@ -39,77 +42,103 @@ func _process(delta):
 	
 	if(pos[X] == row - 1 && pos[Y] == col  - 1):
 		var u_shader = up_arrow.material.get_shader_parameter("rgb")
-		u_shader = Vector4(0,1,0,1)
+		u_shader = sucess_color
 		up_arrow.material.set_shader_parameter("rgb",u_shader)
 		var d_shader = down_arrow.material.get_shader_parameter("rgb")
-		d_shader = Vector4(0,1,0,1)
+		d_shader = sucess_color
 		down_arrow.material.set_shader_parameter("rgb",d_shader)
 		var l_shader = left_arrow.material.get_shader_parameter("rgb")
-		l_shader = Vector4(0,1,0,1)
+		l_shader = sucess_color
 		left_arrow.material.set_shader_parameter("rgb",l_shader)
 		var r_shader = right_arrow.material.get_shader_parameter("rgb")
-		r_shader = Vector4(0,1,0,1)
+		r_shader = sucess_color
 		right_arrow.material.set_shader_parameter("rgb",r_shader)
+		normal_color = sucess_color
 		
 	else:
 		if Input.is_action_just_pressed("ui_up"):
 			up_arrow.position += Vector2(0, -10)
-			if(_move(UP)==-1):
+			var u_move = _move(Direction.UP)
+			if(u_move==-1):
 				var u_shader = up_arrow.material.get_shader_parameter("rgb")
-				u_shader = Vector4(1,0,0,1)
+				u_shader = error_color
+				up_arrow.material.set_shader_parameter("rgb",u_shader)
+			elif(u_move==1):
+				var u_shader = up_arrow.material.get_shader_parameter("rgb")
+				u_shader = door_color
 				up_arrow.material.set_shader_parameter("rgb",u_shader)
 		if Input.is_action_just_released("ui_up"):
 			up_arrow.position += Vector2(0, 10)
 			var u_shader = up_arrow.material.get_shader_parameter("rgb")
-			u_shader = Vector4(1,1,1,1)
+			u_shader = normal_color
 			up_arrow.material.set_shader_parameter("rgb",u_shader)
 			
 		if Input.is_action_just_pressed("ui_down"):
 			down_arrow.position += Vector2(0, 10)
-			if(_move(DOWN)==-1):
+			var d_move = _move(Direction.DOWN)
+			if(d_move==-1):
 				var d_shader = down_arrow.material.get_shader_parameter("rgb")
-				d_shader = Vector4(1,0,0,1)
+				d_shader = error_color
+				down_arrow.material.set_shader_parameter("rgb",d_shader)
+			elif(d_move==1):
+				var d_shader = down_arrow.material.get_shader_parameter("rgb")
+				d_shader = door_color
 				down_arrow.material.set_shader_parameter("rgb",d_shader)
 		if Input.is_action_just_released("ui_down"):
 			down_arrow.position += Vector2(0, -10)
 			var d_shader = down_arrow.material.get_shader_parameter("rgb")
-			d_shader = Vector4(1,1,1,1)
+			d_shader = normal_color
 			down_arrow.material.set_shader_parameter("rgb",d_shader)
 
 		if Input.is_action_just_pressed("ui_left"):
 			left_arrow.position += Vector2(-10, 0)
-			_move(LEFT)
-			if(_move(LEFT)==-1):
+			var l_move = _move(Direction.LEFT)
+			if(l_move==-1):
 				var l_shader = left_arrow.material.get_shader_parameter("rgb")
-				l_shader = Vector4(1,0,0,1)
+				l_shader = error_color
+				left_arrow.material.set_shader_parameter("rgb",l_shader)
+			elif(l_move==1):
+				var l_shader = left_arrow.material.get_shader_parameter("rgb")
+				l_shader = door_color
 				left_arrow.material.set_shader_parameter("rgb",l_shader)
 		if Input.is_action_just_released("ui_left"):
 			left_arrow.position += Vector2(10, 0)
 			var l_shader = left_arrow.material.get_shader_parameter("rgb")
-			l_shader = Vector4(1,1,1,1)
+			l_shader = normal_color
 			left_arrow.material.set_shader_parameter("rgb",l_shader)
 		
 		if Input.is_action_just_pressed("ui_right"):
 			right_arrow.position += Vector2(10, 0)
-			if(_move(RIGHT)==-1):
+			var r_move = _move(Direction.RIGHT)
+			if(r_move==-1):
 				var r_shader = right_arrow.material.get_shader_parameter("rgb")
-				r_shader = Vector4(1,0,0,1)
+				r_shader = error_color
+				right_arrow.material.set_shader_parameter("rgb",r_shader)
+			elif(r_move==1):
+				var r_shader = right_arrow.material.get_shader_parameter("rgb")
+				r_shader = door_color
 				right_arrow.material.set_shader_parameter("rgb",r_shader)
 		if Input.is_action_just_released("ui_right"):
 			right_arrow.position += Vector2(-10, 0)
 			var r_shader = right_arrow.material.get_shader_parameter("rgb")
-			r_shader = Vector4(1,1,1,1)
+			r_shader = normal_color
 			right_arrow.material.set_shader_parameter("rgb",r_shader)
 	
 func _move(dir):
-	if _is_valid_pos(dir):
+	
+	if _is_valid_pos(dir) == 0:
 		pos[X] += dx[dir]
 		pos[Y] += dy[dir]
+		print(pos)
 		return 0
+	print(pos)
+	if _is_valid_pos(dir) == 1:
+		print("Door!")
+		return 1
 	else:
 		print("Block!")
 		return -1
-	print(pos)
+	
 
 func _is_valid_pos(dir):
 	var tmp = [0,0]
@@ -117,9 +146,26 @@ func _is_valid_pos(dir):
 	tmp[Y] = pos[Y] + dy[dir]
 	
 	if (tmp[X] < 0 || tmp[X] >= col || tmp[Y] < 0 || tmp[Y] >= row):
-		return false
+		return -1
 	
-	if (map[tmp[X]][tmp[Y]] == 1):
-		return false
-		
-	return true
+	if (map[tmp[X]][tmp[Y]] == MapType.WALL):
+		return -1
+	if (map[tmp[X]][tmp[Y]] == MapType.DOOR && button_pressed == false):
+		return 1
+	if (map[tmp[X]][tmp[Y]] == MapType.BUTTON && button_pressed == false):
+		button_pressed = true
+		var u_shader = up_arrow.material.get_shader_parameter("rgb")
+		u_shader = door_color
+		up_arrow.material.set_shader_parameter("rgb",u_shader)
+		var d_shader = down_arrow.material.get_shader_parameter("rgb")
+		d_shader = door_color
+		down_arrow.material.set_shader_parameter("rgb",d_shader)
+		var l_shader = left_arrow.material.get_shader_parameter("rgb")
+		l_shader = door_color
+		left_arrow.material.set_shader_parameter("rgb",l_shader)
+		var r_shader = right_arrow.material.get_shader_parameter("rgb")
+		r_shader = door_color
+		right_arrow.material.set_shader_parameter("rgb",r_shader)
+		normal_color = door_color	
+		return 0
+	return 0
