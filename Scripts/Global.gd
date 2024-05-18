48c36895332239bf441d6ad42a95d15efa484806
Darrin-Lin <darrin.de88@gmail.com>
2024-05-19 05:13:28 +0800

    modify: Modify the Map to Globles.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,3 +1,39 @@
 extends Node
 
 var map = 0
+
+# map: 0 empty, -1 wall 
+# door = 2 button = 3
+
+enum DIFF {
+	EASY = 0,
+	NORMAL = 1
+}
+
+var map_eazy = [
+	[0, 1, 0, 0, 0, 0, 0],
+	[0, 1, 1, 1, 0, 1, 0],
+	[0, 0, 0, 1, 0, 1, 0],
+	[1, 1, 0, 1, 1, 1, 0],
+	[0, 0, 0, 1, 0, 0, 0],
+	[0, 1, 1, 1, 0, 1, 0],
+	[0, 0, 0, 0, 0, 1, 0]
+]
+
+var map_normal = [
+	[0, 1, 0, 0, 0, 0, 0, 0, 0],
+	[0, 1, 1, 1, 0, 1, 1, 1, 0],
+	[0, 0, 0, 1, 0, 1, 3, 1, 0],
+	[1, 1, 0, 1, 1, 1, 0, 1, 0],
+	[0, 0, 0, 1, 0, 0, 0, 1, 0],
+	[0, 1, 1, 1, 0, 1, 1, 1, 0],
+	[0, 0, 0, 0, 0, 1, 0, 0, 0],
+	[0, 1, 1, 1, 0, 1, 1, 1, 0],
+	[0, 0, 0, 0, 2, 0, 0, 0, 0]
+]
+
+func select_map(difficulty):
+	if difficulty == DIFF.EASY:
+		return map_eazy
+	elif difficulty == DIFF.NORMAL:
+		return map_normal
